47a0be7aedcdeda5cf777ce1760d9780f65b5747
Martin Peck <mpeck@hotmail.co.uk>
2014-06-11 23:54:35 +0100

    Initial commit of simple Node.js server

---
+++ index.js
@@ -0,0 +1,10 @@
+var server = require("./server");
+var router = require("./router.js");
+var requestHandlers = require("./requestHandlers");
+
+var handle = {};
+handle["/"] = requestHandlers.start;
+handle["/start"] = requestHandlers.start;
+handle["/upload"] = requestHandlers.upload;
+
+server.start(router.route, handle);
